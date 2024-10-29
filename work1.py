9a151ddb1b522b3582bec929242044509bf08334
IkarBogushevich <ilyaboguilyabogushevich114@gmail.com>
2024-10-29 17:17:02 +0300

    Сделана часть кода, думаю над третьей

---
+++ work1.py
@@ -0,0 +1,14 @@
+print ('Hello world') #проба пера
+
+#print("Pls, write your name") # просьба ввода имени
+#name = input() # ввод имени
+#print ("And u surname")# просьба ввода фамилии
+#surname = input()#ввод фамилии
+#print('Hello ' name, surname, "! You just delved into Python! Great start!") #разные выводы
+#print(f'Hello {name}, {surname}, ! You just delved into Python! Great start!') #разные выводы
+
+
+
+#text = input('Enter the text: ')#прошу текст
+#print(text.title())#превращаю текст в нужный
+
