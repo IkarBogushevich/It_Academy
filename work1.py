b0fac9736bdffb7f95b17c0f42487b293b1b1686
IkarBogushevich <ilyaboguilyabogushevich114@gmail.com>
2024-11-17 14:35:13 +0300

    add new doc

--- work1.py
+++ work1.py
@@ -3,7 +3,7 @@ print ('Hello world') #проба пера
 print("Pls, write your name") # просьба ввода имени
 name = input() # ввод имени
 print ("And u surname")# просьба ввода фамилии
-surname = input()#ввод фамилии
+surname = input(ввод фамилии
 print('Hello ', name, surname, "! You just delved into Python! Great start!") #разные выводы
 print(f'Hello {name}, {surname}, ! You just delved into Python! Great start!') #разные выводы
 
